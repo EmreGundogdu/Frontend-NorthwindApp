9f4bb61a3671322dfb40b281bb2aaa9525994f6c
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-17 19:34:09 +0300

    two way binding with categories

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../models/category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -9,9 +10,14 @@ import { Category } from '../models/category';
 export class CategoryComponent implements OnInit {
 
   categories:Category[]=[];
-  constructor() { }
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
-
+  getCategories() {
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data;
+    })
+  }
 }
