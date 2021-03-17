2a3467e59005077b8a713b247cc05e7e8abba7da
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-03-17 22:03:18 +0300

    Added jquerry and A getAllCategories button to categories

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -31,4 +31,12 @@ export class CategoryComponent implements OnInit {
       return "list-group-item"
     }
   }
+  getAllCategoryClass(){
+    if (!this.currentCategory) {
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item"
+    }
+  }
 }
